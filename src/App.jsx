90e46b598ff1c484c6182dbfbecee6b1de309fcd
mihumiko <gavrm04@mail.ru>
2025-02-22 20:40:37 +0300

    learn react day 1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+function App() {
+  
+  return (
+    <div>
+      <header>
+        <div className='head'>
+        < NavBar />
+        </div>
+        <h3>Test №1</h3>
+      </header>
+
+      <main>
+        <h1> Text : dasdadadfhbejqbfqjfbequifbiq</h1>
+      </main>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import './NavBar.css'
+export default function NavBar(){
+    return (
+        <nav>
+            <a href="#">Главная</a>
+            <a href="#">о нас</a>
+            <a href="#">связь</a>
+        </nav>
+    )
+}
