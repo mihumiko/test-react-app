030f020b434268cecbd8cc2a1fdc39b22a12473f
CHIK_PUK_PUK 01 <gavrm04@mail.ru>
2025-03-01 03:05:18 +0300

    h

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -100,6 +100,7 @@ export default function ProductList() {
                   }}
                   sx={{
                     marginLeft: "auto",
+
                     whiteSpace: "nowrap",
                   }}
                 >
